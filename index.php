ead8784cfac7240a64dadf191eceb17fc0513a12
gewehr <gewehr@mail.ru>
2016-10-05 06:30:30 +0400

    create form

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>My homework #2</title>
+	<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+	<div class="content">
+	<div class="send_date">
+	<form action="action.php" method="GET">
+		<input type="text" placeholder="Введите имя" name="name" required><p>
+		<textarea cols="100" rows="5" placeholder="Введите текст сообщения" name="message"></textarea><p>
+		<input type="submit" value="Отправить данные">
+	</form>
+	</div>
+	<fieldset class="messages">
+	<legend>Сообщения</legend>
+	</fieldset>
+	</div>
+</body>
+
+<?php
+
+
+?>
