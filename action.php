5125f81f281886b5bc623be6385e44cb86ae830d
gewehr <gewehr@mail.ru>
2016-10-09 07:52:50 +0400

    finish edit

--- action.php
+++ action.php
@@ -1,58 +1,37 @@
 <?php
 if ($_POST["name"] == ""){
-	$name = "Аноним";
+	header ('Location: index.php?alert=o');
+	exit;
 }
 else{
 $name = trim(htmlspecialchars($_POST["name"]));
 }
 if (isset($_POST["message"])){
-$message = substr(trim(htmlspecialchars($_POST["message"])), 0, 500);
+$message = trim(htmlspecialchars($_POST["message"]));
+$message = mb_substr($message,0,500);
 }
 else{
 	$message = "<Пустое сообщение....>";
 }
-	$month = date("m");
-	switch($month){
-		case 1:
-			$month = "января";
-			break;		
-		case 2:
-			$month = "февраля";
-			break;
-		case 3:
-			$month = "марта";
-			break;
-		case 4:
-			$month = "апреля";
-			break;
-		case 5:
-			$month = "мая";
-			break;
-		case 6:
-			$month = "июня";
-			break;
-		case 7:
-			$month = "июля";
-			break;
-		case 8:
-			$month = "августа";
-			break;
-		case 9:
-			$month = "сентября";
-			break;
-		case 10:
-			$month = "октября";
-			break;
-		case 11:
-			$month = "ноября";
-			break;
-		case 12:
-			$month = "декабря";
-			break;			
-	}
-	$string = "Добавлено ".date("d ").$month.date(" Y")." г. в ".date("H:i:s")."  пользователем <b>$name</b><br>"."Сообщение: $message<p>";
-		$fp = fopen("messages.txt", "a");
-		fwrite($fp, $string);
+$monthEn = strtolower(date('F')); 
+$monthsTranslate = [ 
+'january' => 'января', 
+'february' => 'февраля', 
+'march' => 'марта', 
+'april' => 'апреля', 
+'may' => 'мая', 
+'june' => 'июня', 
+'july' => 'июля', 
+'august' => 'августа', 
+'september' => 'сентября', 
+'october' => 'октября', 
+'november' => 'ноября', 
+'december' => 'декабря', 
+]; 
+$monthRu = $monthsTranslate[$monthEn];
+	$string = array(date(d), $monthRu, date(Y), date("H:i:s"), $name, $message);
+		$fp = fopen("messages.csv", "a");
+		fputcsv($fp, $string);
 		fclose($fp);
 	header ('Location: index.php');
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,14 @@
+<?php
+	if (isset($_GET["alert"])){
+		echo "<script> alert('Не введено имя')</script>";
+	}
+?>
 <!DOCTYPE html>
 <head>
 	<meta charset="utf-8">
 	<title>My homework #2</title>
 	<link rel="stylesheet" href="style.css" type="text/css" />
-	<script id="clientEventHandlersJS" language="javascript">
-<!--
+	<script>
 
 function memo_onkeydown() 
 {
@@ -12,17 +16,14 @@ var s,c;
 s=memo.value;
 c = 500 - s.length;
 countlabel.innerText="Осталось : " + c + " символов";
-
 }
-
-//-->
 </script>
 </head>
 <body>
 	<div class="content">
 	<div class="send_date">
 	<form action="action.php" method="POST">
-		<input type="text" placeholder="Введите имя" name="name" required><p>
+		<input type="text" placeholder="Введите имя" name="name"><p>
 		<textarea cols="100" rows="5" placeholder="Введите текст сообщения" name="message" id="memo" onkeyup="memo_onkeydown()" required></textarea><p>
 		<p id="countlabel">Осталось: 500 символов</p>
 		<input type="submit" value="Отправить данные">
@@ -31,8 +32,10 @@ countlabel.innerText="Осталось : " + c + " символов";
 	<fieldset class="messages">
 	<legend>Сообщения</legend>
 	<?php
-	if (file_exists("messages.txt")){
-		echo file_get_contents('messages.txt');
+	if (file_exists("messages.csv")){
+		$fh = fopen("messages.csv", 'r');
+		while($row = fgetcsv($fh, 0, ','))
+    echo "<b>Добавлено $row[0] $row[1] $row[2] в $row[3] пользователем $row[4]</b> <br> Сообщение: $row[5]<hr><p>";
 	}
 	else echo "Пока записей нет, будь первым!";
 	?>
